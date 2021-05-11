717cdfd7ba017c45bf355980139b2d7a0879b2ee
Sagar Surve <survevishwas60@gmail.com>
2021-05-11 16:12:48 +0530

    Add project files.

---
+++ HealthyMee/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HealthyMee.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace HealthyMee.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
